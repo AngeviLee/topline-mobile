909a9803ec7d9d9d7b04c4fc4ffc1c8209785676
AngeviLee <568178364@qq.com>
2019-07-10 00:28:41 +0800

    登录成功存储用户token到容器中

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// 引入封装操作用户信息的模块
+import * as auth from '@/utils/auth'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    // 初始化的时候从本地存储获取数据，防止刷新token
+    user: auth.getUser()
   },
   mutations: {
-
+    // 登陆成功，调用mutation更新容器中的user的状态
+    setUser (state, data) {
+      state.user = data
+      // 将数据放到本地存储
+      auth.saveUser(state.user)
+    }
   },
   actions: {
 
