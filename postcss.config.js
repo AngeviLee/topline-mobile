2322ee988b57d2d17d0a57735fd669eebb5e8145
AngeviLee <568178364@qq.com>
2019-07-09 22:28:14 +0800

    对vant样式的处理

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,27 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {},
-    'postcss-pxtorem': {
-      rootValue: 37.5,
-      propList: ['*']
+// module.exports = {
+//   plugins: {
+//     autoprefixer: {},
+//     'postcss-pxtorem': {
+//       rootValue: 37.5,
+//       propList: ['*']
+//     }
+//   }
+// }
+const path = require('path')
+
+module.exports = (data) => {
+  // 如果是node_module/vant相关文件，则将rootValue设置为37.5，否则设置为75
+  const rootValue = data.file.dirname.indexOf(`node_modules${path.sep}vant`) !== -1
+    ? 37.5
+    : 75
+
+  return {
+    plugins: {
+      autoprefixer: {},
+      'postcss-pxtorem': {
+        rootValue,
+        propList: ['*']
+      }
     }
   }
 }
