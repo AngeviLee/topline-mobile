cab901b0afa1f0251c08e47a210b7d506f6ef208
AngeviLee <568178364@qq.com>
2019-07-15 22:49:37 +0800

    搜索-准备页面

---
+++ src/views/search/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- 搜索框 -->
+    <van-search placeholder="请输入搜索关键词" v-model="searchText" />
+    <!-- 搜索框 -->
+    <!-- 联想建议 -->
+    <van-cell-group>
+      <van-cell title="hello" icon="search" />
+      <van-cell title="hello" icon="search" />
+      <van-cell title="hello" icon="search" />
+      <van-cell title="hello" icon="search" />
+      <van-cell title="hello" icon="search" />
+      <van-cell title="hello" icon="search" />
+      <van-cell title="hello" icon="search" />
+      <van-cell title="hello" icon="search" />
+    </van-cell-group>
+    <!-- 联想建议 -->
+
+    <!-- 历史记录 -->
+    <!-- 历史记录 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchIndex',
+  data () {
+    return {
+      searchText: ''
+    }
+  },
+  components: {}
+}
+</script>
+
+<style lang='less' scoped>
+</style>
