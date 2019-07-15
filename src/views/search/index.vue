de76071b604b3344e61ea5769f6727d301379cd8
AngeviLee <568178364@qq.com>
2019-07-15 23:14:55 +0800

    搜索-联想建议-获取并展示

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -5,14 +5,12 @@
     <!-- 搜索框 -->
     <!-- 联想建议 -->
     <van-cell-group>
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
-      <van-cell title="hello" icon="search" />
+      <van-cell
+        v-for="item in suggestion"
+        :key="item"
+        :title="item"
+        icon="search"
+      />
     </van-cell-group>
     <!-- 联想建议 -->
 
@@ -22,11 +20,35 @@
 </template>
 
 <script>
+// 引入封装的联想建议框
+import { getSuggestion } from '@/api/serach'
+
 export default {
   name: 'SearchIndex',
   data () {
     return {
-      searchText: ''
+      // 搜索框的关键词内容
+      searchText: '',
+      // 声明一个数组存放联想建议
+      suggestion: []
+    }
+  },
+  watch: {
+    // 监视输入数据的改变，当数据发生变化，发请求获取搜索建议，展示到列表中
+    async searchText (text) {
+      // 去除首尾空格
+      text = text.trim()
+      // 非空判断
+      if (!text.length) {
+        return
+      }
+      // 请求获取建议
+      try {
+        const data = await getSuggestion(text)
+        this.suggestion = data.options
+      } catch (err) {
+        console.log(err)
+      }
     }
   },
   components: {}
