a4664d1b363256002bceb1bfdab1fb0ab12f3c69
AngeviLee <568178364@qq.com>
2019-07-22 21:41:16 +0800

    实现token过期刷新token功能

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 import store from '@/store'
 // 引入处理超出整数范围的大数字的第三方包
 import JSONbig from 'json-bigint'
+// 引入路由文件
+import router from '@/router'
 
 // axios.create用于创建一个axios实例，该实例和axios的功能是一模一样的
 // 说白了，这样就是克隆了一个axios
@@ -44,8 +46,45 @@ request.interceptors.response.use(function (response) {
   // 如果响应结果对象中有data，则直接返回这个data数据
   // 如果响应结果对象中没有data，则不作任何处理，原样返回这个数据
   return response.data.data || response.data
-}, function (error) {
-  // Do something with response error
+}, async error => {
+  if (error.response.status === 401) {
+    // 看一下有没有user（有user就有refresh_token)
+    const user = store.state.user
+    // 如果连user都没有，则直接跳转登录页去登陆
+    if (!user) {
+      router.push({
+        name: 'login'
+      })
+      return
+    }
+    // 如果有user,我们请求接口使用refresh_token获取新的访问token
+    try {
+      // 注意： 请求获取刷新token不要使用request
+      // 因为刷新token有他自己的处理流程
+      // 如果你使用request的话，需要做更多的判断处理，会导致你分不清请求的流程了
+      const { data } = await axios({
+        method: 'PUT',
+        url: 'http://ttapi.research.itcast.cn/app/v1_0/authorizations',
+        headers: {
+          Authorization: `Bearer ${user.refresh_token}`
+        }
+      })
+      // 拿到新的登录令牌了
+      // 更新用户token
+      store.commit('setUser', {
+        token: data.data.token,
+        refresh_token: user.refresh_token
+      })
+      // 把本次因为token过期的请求继续发出去
+      // 非刷新token的请求,使用request走我们的那个正常的请求拦截相关流程
+      return request(error.config)
+    } catch (refreshErr) {
+      // 刷新token都请求失败,甭想了，直接去登录页
+      router.push({
+        name: 'login'
+      })
+    }
+  }
   return Promise.reject(error)
 })
 
